ccca881272907832c6f616074ac0cca28f035625
Gonzalo L Canut <glcanut@gmail.com>
2024-08-02 19:04:16 -0300

    styles, edit and delete function added

--- Assets/js/add.js
+++ Assets/js/add.js
@@ -1,9 +1,19 @@
-document.getElementById("submit-button").addEventListener("click", saveData);
-document.getElementById("cancel-button").addEventListener("click", clearForm);
-
+let id = 1;
 let cards = JSON.parse(localStorage.getItem("add")) || [];
+let cardToEdit = JSON.parse(localStorage.getItem("edit"));
 
-function saveData() {
+if (cardToEdit) {
+  document.getElementById("fName").value = cardToEdit.name;
+  document.getElementById("fAuthor").value = cardToEdit.autor;
+  document.getElementById("fCategory").value = cardToEdit.category;
+  document.getElementById("fDescription").value = cardToEdit.description;
+
+  localStorage.removeItem("edit");
+  cards = cards.filter((element) => element.id !== id);
+  localStorage.setItem("add", JSON.stringify(cards));
+}
+
+const saveData = () => {
   let name = document.getElementById("fName").value;
   let autor = document.getElementById("fAuthor").value;
   let category = document.getElementById("fCategory").value;
@@ -15,18 +25,20 @@ function saveData() {
     return;
   }
 
-  let card = { name, autor, category, description, image };
+  let card = { id, name, autor, category, description, image };
 
   cards.push(card);
 
   localStorage.setItem("add", JSON.stringify(cards));
 
-  clearForm();
-}
+  id++;
 
-function clearForm() {
+  clearForm();
+};
+
+const clearForm = () => {
   document.getElementById("fName").value = "";
   document.getElementById("fAuthor").value = "";
   document.getElementById("fCategory").value = "";
   document.getElementById("fDescription").value = "";
-}
+};
--- Assets/js/collection.js
+++ Assets/js/collection.js
@@ -1,4 +1,5 @@
 let cards = JSON.parse(localStorage.getItem("add")) || [];
+let id = 0;
 
 const rendercards = (arrayCards) => {
   let containerCards = document.getElementById("cardContainer");
@@ -6,14 +7,41 @@ const rendercards = (arrayCards) => {
 
   arrayCards.forEach((card) => {
     let dataCard = document.createElement("div");
-    dataCard.className = "card";
-    dataCard.innerHTML = `<img src=${card.image} />
-            <h3>${card.autor}</h3>
-            <p>${card.category}</p>
-            <p>${card.description}</p>
+    dataCard.className = "card mb-3";
+    dataCard.innerHTML = `
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src=${card.image} class="img-fluid rounded-start collectionImage" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body d-flex flex-column">
+                  <h5 class="card-title">${card.name}</h5>
+                  <p class="card-text">Author: ${card.autor}</p>
+                  <p class="card-text">Category: ${card.category}</p>
+                  <p class="card-text">Category: ${card.description}</p>
+                  <div class="collectionIcons d-flex justify-content-end">
+                      <i onclick="editCard(${card.id})" class="bi bi-pencil-fill me-2"></i>
+                      <i onclick="deleteCard(${card.id})" class="bi bi-trash-fill me-2"></i>
+                  </div>
+              </div>
+            </div>
+          </div>
         `;
+
     containerCards.appendChild(dataCard);
   });
 };
 
 rendercards(cards);
+
+const deleteCard = (id) => {
+  cards = cards.filter((element) => element.id !== id);
+  localStorage.setItem("add", JSON.stringify(cards));
+  rendercards(cards);
+};
+
+const editCard = (id) => {
+  card = cards.find((element) => element.id === id);
+  localStorage.setItem("edit", JSON.stringify(card));
+  window.location.href = "add.html";
+};
